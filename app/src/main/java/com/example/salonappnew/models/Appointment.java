9b650e131142dfe9d6e9c6178ddd14080efa620c
sachitha2 <shpsachitha@gmail.com>
2020-10-04 00:35:26 +0530

    Appointment model created

--- app/src/main/java/com/example/salonappnew/models/Appointment.java
+++ app/src/main/java/com/example/salonappnew/models/Appointment.java
@@ -2,31 +2,71 @@ package com.example.salonappnew.models;
 
 public class Appointment {
 
-    String salon;
+    String salonId;
+    String salonEmail;
     String date;
     String time;
+    String customerId;
+    String customerEmail;
 
-    public String getSalon() {
-        return salon;
+    public Appointment(String salonId, String salonEmail, String date, String time, String customerId, String customerEmail) {
+        this.salonId = salonId;
+        this.salonEmail = salonEmail;
+        this.date = date;
+        this.time = time;
+        this.customerId = customerId;
+        this.customerEmail = customerEmail;
+    }
+
+    public Appointment(){
+
+    }
+
+    public String getSalonId() {
+        return salonId;
+    }
+
+    public void setSalonId(String salonId) {
+        this.salonId = salonId;
+    }
+
+    public String getSalonEmail() {
+        return salonEmail;
+    }
+
+    public void setSalonEmail(String salonEmail) {
+        this.salonEmail = salonEmail;
     }
 
     public String getDate() {
         return date;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getTime() {
         return time;
     }
 
-
-
-    public Appointment(){
-
+    public void setTime(String time) {
+        this.time = time;
     }
 
-    public Appointment(String salon, String date, String time) {
-        this.salon = salon;
-        this.date = date;
-        this.time = time;
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
     }
 }

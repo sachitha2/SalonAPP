d9621ea633eaae767f81a3b554156481a2bdcf73
sachitha2 <shpsachitha@gmail.com>
2020-10-05 23:49:03 +0530

    Salon appoinments show UI done

--- app/src/main/java/com/example/salonappnew/ui/SalonAppoinmentShow.java
+++ app/src/main/java/com/example/salonappnew/ui/SalonAppoinmentShow.java
@@ -1,15 +1,23 @@
 package com.example.salonappnew.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.salonappnew.R;
+import com.example.salonappnew.about;
 import com.example.salonappnew.models.CustomerAppoinment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +30,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class SalonAppoinmentShow extends AppCompatActivity  implements TextWatcher {
+
+    DrawerLayout drawerLayout;
 
     ArrayList<CustomerAppoinment> myList;
     SalonAppoinmentAdapter myAdapter;
@@ -36,6 +46,10 @@ public class SalonAppoinmentShow extends AppCompatActivity  implements TextWatch
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salon_appoinment_show);
+
+        //nav start
+        drawerLayout = findViewById(R.id.drawer_layout);
+        //nav end
 
         searchCustomers = findViewById(R.id.txtsearchCustomers);
 
@@ -103,4 +117,76 @@ public class SalonAppoinmentShow extends AppCompatActivity  implements TextWatch
             }
         });
     }
+
+    //nav start
+
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickLogout(View view){
+        Log.d("Data","Log out clicked");
+
+        logout(this);
+    }
+
+    public void ClickHome(View view) {
+        redirectActivity(this, Dashboard.class);
+    }
+    public void ClickDashboard(View view){
+        redirectActivity(this, Dashboard.class);
+
+    }
+    public void ClickAboutUs(View view){
+        redirectActivity(this, about.class);
+    }
+
+    public static void logout(final Activity activity){
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you Sure youb want to logout?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth.getInstance().signOut();
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+    //nav end
 }

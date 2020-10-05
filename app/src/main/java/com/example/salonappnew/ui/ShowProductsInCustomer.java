7997d34f681a98b0e14156432b45d677139c8349
sachitha2 <shpsachitha@gmail.com>
2020-10-05 21:32:01 +0530

    show product in customer init

---
+++ app/src/main/java/com/example/salonappnew/ui/ShowProductsInCustomer.java
@@ -0,0 +1,14 @@
+package com.example.salonappnew.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowProductsInCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_products_in_customer);
+    }
+}

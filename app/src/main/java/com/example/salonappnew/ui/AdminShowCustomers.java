59b28a2fed5b6a7a58fc0bd3a351620bac502c9a
sachitha2 <shpsachitha@gmail.com>
2020-10-06 15:33:34 +0530

    Dash board added to admincompany show and admin customer show

--- app/src/main/java/com/example/salonappnew/ui/AdminShowCustomers.java
+++ app/src/main/java/com/example/salonappnew/ui/AdminShowCustomers.java
@@ -1,16 +1,104 @@
 package com.example.salonappnew.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.salonappnew.R;
+import com.example.salonappnew.about;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminShowCustomers extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_show_customers);
+
+        //nav start
+        drawerLayout = findViewById(R.id.drawer_layout);
+        //nav end
     }
+
+    //nav start
+
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickLogout(View view){
+        Log.d("Data","Log out clicked");
+
+        logout(this);
+    }
+
+    public void ClickHome(View view) {
+        recreate();
+    }
+    public void ClickDashboard(View view){
+        recreate();
+
+    }
+    public void ClickAboutUs(View view){
+        redirectActivity(this, about.class);
+    }
+
+    public static void logout(final Activity activity){
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you Sure youb want to logout?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth.getInstance().signOut();
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+    //nav end
 }

6a4b9ae5d4af66b63f5774a79553e7d30cbec8a1
sachitha2 <shpsachitha@gmail.com>
2020-10-06 20:25:17 +0530

    set image done in add customer

--- app/src/main/java/com/example/salonappnew/addcustomer.java
+++ app/src/main/java/com/example/salonappnew/addcustomer.java
@@ -3,11 +3,14 @@ package com.example.salonappnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -29,6 +32,12 @@ public class addcustomer extends AppCompatActivity {
     boolean gender;
     RadioButton gMale;
     RadioButton gFmale;
+
+
+    ImageView imageView;
+    Button button1;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,7 +81,33 @@ public class addcustomer extends AppCompatActivity {
         userId = mFDb.push().getKey();
         Log.d("Add Customer ","user id is "+userId);
 
+
+        imageView = (ImageView)findViewById(R.id.img);
+        button1 = (Button)findViewById(R.id.btn_img);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
     }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
     public void openDashBoard(){
         Intent intent = new Intent(this, Dashboard.class);
         intent.putExtra("type","CUSTOMER");

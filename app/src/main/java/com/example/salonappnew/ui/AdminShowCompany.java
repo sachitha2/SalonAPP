ea8b31ddd1a318ab1662c320f54b8be770aca928
sachitha2 <shpsachitha@gmail.com>
2020-10-06 15:26:26 +0530

    Admin show customers done Admin show company init done

---
+++ app/src/main/java/com/example/salonappnew/ui/AdminShowCompany.java
@@ -0,0 +1,15 @@
+package com.example.salonappnew.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.salonappnew.R;
+
+public class AdminShowCompany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_show_company);
+    }
+}
---
+++ app/src/main/java/com/example/salonappnew/ui/AdminShowCustomers.java
@@ -0,0 +1,16 @@
+package com.example.salonappnew.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.salonappnew.R;
+
+public class AdminShowCustomers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_show_customers);
+    }
+}

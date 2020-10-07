3cc81df6ea72e6c04d43c403195e90346a3fd53a
sachitha2 <shpsachitha@gmail.com>
2020-10-07 21:36:24 +0530

    About us error resolved

--- app/src/main/java/com/example/salonappnew/about.java
+++ app/src/main/java/com/example/salonappnew/about.java
@@ -2,6 +2,7 @@ package com.example.salonappnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +10,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.salonappnew.common.Common;
 import com.example.salonappnew.ui.Dashboard;
 
 public class about extends AppCompatActivity {
+
+    //Find profile data start
+    static ImageView imgPropic;
+    static TextView txtProfileName;
+    static String type;
+    //Find profile data end
     DrawerLayout drawerLayout;
 
     @Override
@@ -25,19 +36,34 @@ public class about extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        //Find profile data start
+        imgPropic = findViewById(R.id.imgProfile);
+        txtProfileName = findViewById(R.id.txtProfileName);
+
+        Intent intent = getIntent();
+        Log.d("Data",   "Intent select a salon "+intent.getStringExtra("type"));
+//        type = intent.getStringExtra("type");
+
+        //Find profile data end
+
         drawerLayout = findViewById(R.id.drawer_layout);
     }
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+        //Find profile data start
+        Common.findProfileImg(imgPropic,type,txtProfileName);
+        //Find profile data end
+    }
+
     public void ClickMenu(View view){
         drawer.openDrawer(drawerLayout);
     }
     public void ClickLogo(View view){
         drawer.closeDrawer(drawerLayout);
     }
-    public void ClickHome(View view){
-        drawer.redirectActivity(this, Dashboard.class);
-    }
     public void ClickDashboard(View view){
-        drawer.redirectActivity(this,Dashboard.class);
+        Common.dashboardOpen(this,Dashboard.class,"CUSTOMER");
     }
     public void ClickAboutUs(View view){
 

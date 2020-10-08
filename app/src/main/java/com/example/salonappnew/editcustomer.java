c62bafb49d2ec92cde1dfcdaff944347ebf127ec
sachitha2 <shpsachitha@gmail.com>
2020-10-08 16:25:14 +0530

    customer edit part done

--- app/src/main/java/com/example/salonappnew/editcustomer.java
+++ app/src/main/java/com/example/salonappnew/editcustomer.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -26,9 +27,15 @@ import com.example.salonappnew.common.Common;
 import com.example.salonappnew.models.Customer;
 import com.example.salonappnew.models.UserType;
 import com.example.salonappnew.ui.Dashboard;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,8 +43,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 public class editcustomer extends AppCompatActivity {
 
@@ -45,6 +56,8 @@ public class editcustomer extends AppCompatActivity {
     static ImageView imgPropic,imgPropicEdit;
     static TextView txtProfileName;
     static String type;
+    private  String userId;
+    private  String imageUrl;
     //Find profile data end
 
     DrawerLayout drawerLayout;
@@ -56,6 +69,9 @@ public class editcustomer extends AppCompatActivity {
     EditText name;
     EditText txtEmail;
     EditText phone;
+    EditText txtPassConfirm;
+    EditText txtPass;
+    EditText txtPassOld;
 
     Button edit,del,changeImage;
 
@@ -66,6 +82,11 @@ public class editcustomer extends AppCompatActivity {
     RadioButton gFmale;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
+
+
+    String sPass;
+    String sPassC;
+    String sPassOld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,6 +121,9 @@ public class editcustomer extends AppCompatActivity {
         name = findViewById(R.id.txtName);
         txtEmail = findViewById(R.id.txtEmail);
         phone = findViewById(R.id.txtPhone);
+        txtPass = findViewById(R.id.txtPass);
+        txtPassConfirm = findViewById(R.id.txtPassConfirm);
+        txtPassOld = findViewById(R.id.txtPassOld);
 
         txtEmail.setEnabled(false);
 
@@ -109,9 +133,38 @@ public class editcustomer extends AppCompatActivity {
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Customer customer = new Customer(name.getText().toString(),phone.getText().toString(),mFirebaseAuth.getCurrentUser().getEmail(),"","");
-                mFDb.child("customer").child(key).setValue(customer);
-                Toast.makeText(getApplicationContext(), "Profile updated", Toast.LENGTH_LONG).show();
+                sPass = txtPass.getText().toString();
+                sPassC = txtPassConfirm.getText().toString();
+                sPassOld = txtPassOld.getText().toString();
+
+                //check password status
+                if(sPass.isEmpty() && sPassC.isEmpty() && sPassOld.isEmpty()){
+                    //change data with out changing password
+                    Log.d("Data","All password fields are empty");
+                    uploadImage();
+                }else{
+                    //now check password status
+                    Log.d("Data","password checking");
+                    if(passChack()){
+                        //pass check done
+                        Log.d("Data","Password ok");
+                        //code here
+                        changePassword();
+                    }
+                }
+
+//
+
+//                String gen;
+//                if(gMale.isChecked()){
+//                    gen = "Male";
+//                }else{
+//                    gen = "Female";
+//                }
+//
+//                Customer customer = new Customer(name.getText().toString(),phone.getText().toString(),mFirebaseAuth.getCurrentUser().getEmail(),gen,"",imageUri.toString());
+//                mFDb.child("customer").child(key).setValue(customer);
+//                Toast.makeText(getApplicationContext(), "Profile updated", Toast.LENGTH_LONG).show();
 
 
             }
@@ -170,12 +223,17 @@ public class editcustomer extends AppCompatActivity {
                 System.out.println(dataSnapshot.getKey());
 
                 Log.d("Data","Data ->"+dataSnapshot.getKey());
+                userId = dataSnapshot.getKey();
+                Log.d("Data","User id is ->"+userId);
                 key = dataSnapshot.getKey();
+
                 Log.d("Data","Data ->"+dataSnapshot.getValue().toString());
 
                 Customer customer = dataSnapshot.getValue(Customer.class);
 
                 Log.d("Data","Customer "+customer.getName());
+
+                imageUrl = customer.getImgUrl();
 
                 name.setText(customer.getName());
                 txtEmail.setText(customer.getEmail());
@@ -297,4 +355,142 @@ public class editcustomer extends AppCompatActivity {
     }
 
     //nav end
+
+
+    public  void changePassword(){
+        String email = txtEmail.getText().toString();
+        final String pass = txtPass.getText().toString();
+        final String passOld = txtPassOld.getText().toString();
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(email, passOld);
+
+// Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            user.updatePassword(pass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("Data", "Password updated");
+                                        Toast.makeText(editcustomer.this,"Password updated",Toast.LENGTH_LONG).show();
+                                        uploadImage();
+                                    } else {
+                                        Toast.makeText(editcustomer.this,"Error password not updated",Toast.LENGTH_LONG).show();
+                                        Log.d("Data", "Error password not updated");
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(editcustomer.this,"Error auth failed",Toast.LENGTH_LONG).show();
+                            Log.d("Data", "Error auth failed");
+                        }
+                    }
+                });
+    }
+
+    public boolean passChack(){
+        String sPold;
+        String sPnew;
+        String sPnewConfirm;
+
+        sPold = txtPassOld.getText().toString();
+        sPnew = txtPass.getText().toString();
+        sPnewConfirm = txtPassConfirm.getText().toString();
+
+        if(sPold.isEmpty()){
+            txtPassOld.setError("Please provide your Old Password");
+            txtPassOld.requestFocus();
+            return false;
+        }else if(sPnew.isEmpty()){
+            txtPass.setError("Please provide your New Password");
+            txtPass.requestFocus();
+            return false;
+        }else if(sPnewConfirm.isEmpty()){
+            txtPassConfirm.setError("Please confirm your New Password");
+            txtPassConfirm.requestFocus();
+            return false;
+        }
+        else if(sPnew.length() < 6 && sPassC.length() < 6){
+            Toast.makeText(editcustomer.this,"Min password length is 6 characters",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(!sPnew.equals(sPnewConfirm)){
+            Toast.makeText(editcustomer.this,"Please Enter same password in both fields",Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return  true;
+        }
+
+    }
+    private void uploadImage() {
+
+        Log.d("Data","image uri"+imageUri);
+        if(imageUri != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            final String imgId = "images/"+ UUID.randomUUID().toString();
+            StorageReference ref = storageReference.child(imgId);
+            Log.d("Data","uri"+imageUri.toString());
+            ref.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            addData(imgId);
+                            progressDialog.dismiss();
+                            Toast.makeText(editcustomer.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(editcustomer.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }else{
+            addData(imageUrl);
+        }
+    }
+
+
+
+    public void addData(String imageUrl){
+        String nameS;
+        String phoneS;
+        String email;
+        String gen;//false for women // true for men
+
+        String password;
+
+        nameS = name.getText().toString();
+        phoneS = phone.getText().toString();
+        email = txtEmail.getText().toString();
+
+        //take gender
+        if(gMale.isChecked()){
+            gen = "Male";
+        }else{
+            gen = "Female";
+        }
+
+
+
+        Customer customer = new Customer(nameS,phoneS,email,gen,"NO",imageUrl);
+        mFDb.child("customer").child(userId).setValue(customer);
+    }
 }

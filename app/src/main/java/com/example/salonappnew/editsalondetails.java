ab093f5211a68e511f7615777bd7bb06b203e5b8
sachitha2 <shpsachitha@gmail.com>
2020-10-08 17:13:46 +0530

    Salon edit page wip

--- app/src/main/java/com/example/salonappnew/editsalondetails.java
+++ app/src/main/java/com/example/salonappnew/editsalondetails.java
@@ -12,6 +12,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -55,9 +56,12 @@ public class editsalondetails extends AppCompatActivity {
     EditText address;
     EditText phone;
 
-    Button edit,del;
+    Button edit,del,changeImage;
 
     String key;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,9 +119,33 @@ public class editsalondetails extends AppCompatActivity {
             }
         });
 
+
+
+        changeImage = (Button)findViewById(R.id.btn_img);
+
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
         searchFireStore();
     }
 
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imgPropicEdit.setImageURI(imageUri);
+        }
+    }
 
 
     private void searchFireStore(){
